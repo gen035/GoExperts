4acfb6ec71a73658874ab9e8e9c7c902f649128a
Genevieve Perron-Migneron <info@gen-migneron.com>
2020-01-12 18:55:56 -0500

    fix styling

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,4 +1,15 @@
 // This is where it all goes :)
+
 function menu() {
   document.body.classList.toggle('is-menu-opened');
 };
+
+window.addEventListener('scroll', function() {
+	var element = document.querySelector('footer');
+  var position = element.getBoundingClientRect();
+
+	// checking for partial visibility
+	if(position.top < window.innerHeight && position.bottom >= 0) {
+		element.classList.add('is-visible');
+	}
+});
